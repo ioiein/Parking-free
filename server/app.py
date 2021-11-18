5e59e7347dc47fd3f349856fda647e83df64fafb
ioiein <71278916+ioiein@users.noreply.github.com>
2021-11-18 06:58:34 +0300

    fixed path

--- server/app.py
+++ server/app.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 @app.get("/")
 async def main():
-    return FileResponse('img/smth.jpg')
+    return FileResponse('img/out.jpg')
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=os.getenv("PORT", 8000))
