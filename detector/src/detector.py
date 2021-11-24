447bc1b1fe0d77d64556d4191e1c6085c27c8eff
ioiein <71278916+ioiein@users.noreply.github.com>
2021-11-24 16:33:09 +0300

    added ffmpeg command

--- detector/src/detector.py
+++ detector/src/detector.py
@@ -5,6 +5,7 @@ import click
 import torch
 import json
 import cv2
+import os
 
 
 LOGGER_CFG = "configs/logging.conf.yaml"
@@ -34,6 +35,8 @@ def draw_bbox(marks, image_path=IMG_PATH, output_img=OUTPUT_IMG):
 
 @click.command(name="detect")
 def detect_command():
+    os.system(
+        f"ffmpeg -i https://msk.rtsp.me/XEmxGcyEbsWZaHxQlTe5-w/1635357896/hls/ZdG5F8D5.m3u8 -frames:v 1 {IMG_PATH}.jpg")
     setup_logging(LOGGER_CFG)
     logger.info(f"script started")
     try:
